788a1bf9a882beaf315e1f196b850a7ed52d7e5c
Carlo Costantino <costantinoart@gmail.com>
2020-03-26 01:07:08 -1000

    Styled sections and added new landing page text

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -6,74 +6,61 @@ function Landing(props) {
     <>
       <header role="banner">
         <h1>Listen and Share</h1>
+        <p>In the age of Social Media we are more dissconnected than ever.</p>
         <h2>
-          A place to express yourself anonymusly with no profiles, no likes, no
+          A place to express yourself anonymously with no profiles, no likes, no
           followers
         </h2>
-        <p>
-          Personal message about I created this and what I want people to get
-          out of it
-        </p>
       </header>
       <section>
         <header>
-          <h3>First, emotional awareness</h3>
+          <h3>Identify what you're feeling</h3>
         </header>
-        <p>
-          [<em>placeholder for quotes and information about grounding</em>]
-        </p>
         <p>
           You'll go through a process to move into your physical body and get
-          out of your head. Being present in your body allows you to get in
-          touch with the feelings you are experiencing
+          out of your head.
+        </p>
+        <p>
+          Being present in your body allows you to get in touch with the
+          feelings you are experiencing
         </p>
       </section>
       <section>
         <header>
-          <h3>Listen to others</h3>
+          <h3>Listen to humans that are going through similar things</h3>
         </header>
         <p>
-          [
-          <em>
-            placeholder for quotes and info about the importance of listening
-          </em>
-          ]
+          Find videos and stories of people experiencing and expressing their
+          emotions.
         </p>
-        <p>
-          Find videos and stories of people experiencing and expressing emotions
-          simmilar to yours.
-        </p>
+        <p>You may hear/read something you can relate to.</p>
       </section>
       <section>
         <header>
-          <h3>Share yourself</h3>
+          <h3>Share your emotional experiences with other humans</h3>
         </header>
         <p>
-          [
-          <em>
-            placeholder for quotes and info about the benefits of vulnerability
-            and sharing with others
-          </em>
-          ]
-        </p>
-        <p>
-          Optional, this gives you an opportunity to voice your emotional
-          experiences. In doing so, feel the relief/inner space that is created
-          when fully agnolaging and expressing whats going on inside.
+          It's a very scary thing to do. (or at least it is for me) This gives
+          you an opportunity to anonymously voice your emotional experiences. I
+          know for me, I get a sense of relief when I fully acknowledge and
+          express what's going on inside.
         </p>
       </section>
       <section>
-        <h2>Guidelines about the app in general</h2>
+        <h2>Guidelines</h2>
+        <p>We keep this a safe space by self monitoring.</p>
+        <p>Please, when sharing, do not use names or details of others.</p>
+        <p>Speak only to your own experience.</p>
       </section>
       <section>
         <p>
           Warning: This app is not for everybody. It contains content that may
           be disturbing and upsetting. If at any point you feel yourself being
-          triggered, take a breath starting in your belly and reach out for help
+          triggered, take a deep breathe and reach out for help
         </p>
-        <header>
-          <h3>Take a deep breath, from your belly...</h3>
-        </header>
+        <p>
+          Use the "Saftey" button if you are feeling like you may harm yourself.
+        </p>
       </section>
       <section>
         <h3>Begin</h3>
