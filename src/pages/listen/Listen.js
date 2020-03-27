55cbd2f2d1947bad77503a56b0992faea53a09c5
Carlo Costantino <costantinoart@gmail.com>
2020-03-27 13:25:41 -1000

    Added styling, example share, and moved buttons

--- src/pages/listen/Listen.js
+++ src/pages/listen/Listen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../../components/button/Button";
+import "./listen.css";
 
 class Listen extends React.Component {
   render() {
@@ -9,22 +10,38 @@ class Listen extends React.Component {
           <h1>Listen</h1>
         </header>
         <section>
-          <p>
-            - This section re-states the emotions that the listener was feeling
+          <h2>A black feeling of anxiety</h2>
+          <p className="share-text">Quarantine.</p>
+          <p className="share-text">
+            I've been in a house with my family for two
+            weeks straight has me feeling stressed out and anxious.
           </p>
-          <p>
-            - Displays the intention of the person sharing the text/audio/video
+          <p className="share-text">
+            Tentions are running high and my desire is to fix it.
+          </p>
+          <p className="share-text">
+            I am a peace-keeper and I am feeling like I'm not in control.
+          </p>
+          <p className="share-text">
+            Not in control of anything in my life right now.
+          </p>
+          <p className="share-text">
+            Although I'm getting help, I feel like the
+            responsiblities of the house fall on my shoulders.
+          </p>
+          <p className="share-text">Is that true?</p>
+          <p className="share-text">
+            Do all of these responsibilities fall on me?
+          </p>
+          <p className="share-text">If not, what is true?</p>
+          <p className="share-text">
+            It's hard for me to tell while I still feel this black feeling in my
+            stomach.
           </p>
         </section>
         <section>
-          <p>
-            This is a placeholder for a conditionally rendering div that will
-            display either text, video, or audio.
-          </p>
-        </section>
-        <section>
-          <Button buttonText="Share"></Button>
           <Button buttonText="Next"></Button>
+          <Button buttonText="Share"></Button>
         </section>
       </>
     );
