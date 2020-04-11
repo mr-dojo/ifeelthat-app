96446aabc3060ff5a929e76a6b37c9bc20240c19
Carlo Costantino <costantinoart@gmail.com>
2020-04-10 18:07:19 -1000

    Fixed className

--- src/pages/listen/listenText/ListenText.js
+++ src/pages/listen/listenText/ListenText.js
@@ -1,31 +1,34 @@
 import React from "react";
 
-export default function ShareText(props) {
+export default function ListenText(props) {
+  console.log(props.url);
   return (
     <section>
       <h2>A black feeling of anxiety</h2>
-      <p className="share-text">Quarantine.</p>
-      <p className="share-text">
+      <p className="listen-text">Quarantine.</p>
+      <p className="listen-text">
         I've been in a house with my family for two
         weeks straight has me feeling stressed out and anxious.
       </p>
-      <p className="share-text">
+      <p className="listen-text">
         Tentions are running high and my desire is to fix it.
       </p>
-      <p className="share-text">
+      <p className="listen-text">
         I am a peace-keeper and I am feeling like I'm not in control.
       </p>
-      <p className="share-text">
+      <p className="listen-text">
         Not in control of anything in my life right now.
       </p>
-      <p className="share-text">
+      <p className="listen-text">
         Although I'm getting help, I feel like the
         responsiblities of the house fall on my shoulders.
       </p>
-      <p className="share-text">Is that true?</p>
-      <p className="share-text">Do all of these responsibilities fall on me?</p>
-      <p className="share-text">If not, what is true?</p>
-      <p className="share-text">
+      <p className="listen-text">Is that true?</p>
+      <p className="listen-text">
+        Do all of these responsibilities fall on me?
+      </p>
+      <p className="listen-text">If not, what is true?</p>
+      <p className="listen-text">
         It's hard for me to tell while I still feel this black feeling in my
         stomach.
       </p>
