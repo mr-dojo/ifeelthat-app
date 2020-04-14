fe72bf1807c8252f219bd86f2aa15c96e4fdb20d
Carlo Costantino <costantinoart@gmail.com>
2020-04-14 09:51:06 -1000

    Updated API_ENDPOINT for production

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  API_ENDPOINT: "http://localhost:8000/",
+  API_ENDPOINT:
+    process.env.NODE_ENV === "production"
+      ? "https://ifeelthat-api.herokuapp.com/"
+      : "http://localhost:8000/",
 };
