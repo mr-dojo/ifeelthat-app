34b876ba9ec223b1384132ba1c45aeccfd4b58a1
Carlo Costantino <costantinoart@gmail.com>
2020-04-15 12:53:56 -1000

    Implimented simple safety button

--- src/components/safety/Safety.js
+++ src/components/safety/Safety.js
@@ -1,12 +1,52 @@
 import React from "react";
 import Button from "../button/Button";
 
-function Safety(props) {
-  return (
-    <>
-      <Button buttonText="Safety"></Button>
-    </>
-  );
+class Safety extends React.Component {
+  state = {
+    buttonPressed: false,
+  };
+
+  handleButtonPress = () => {
+    console.log("button was pressed");
+    this.setState({
+      buttonPressed: !this.state.buttonPressed,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.buttonPressed ? (
+          <section>
+            <h2>Reach out for help, you arn't alone</h2>
+            <a
+              href="tel:+18002738255"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Call Lifeline
+            </a>
+            <a
+              href="https://suicidepreventionlifeline.org/chat/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Chat with somebody
+            </a>
+            <Button
+              buttonText="Close"
+              onClick={() => this.handleButtonPress()}
+            />
+          </section>
+        ) : (
+          <Button
+            buttonText="Safety"
+            onClick={() => this.handleButtonPress()}
+          />
+        )}
+      </>
+    );
+  }
 }
 
 export default Safety;
