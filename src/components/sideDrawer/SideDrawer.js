78047cec502fd376570e4d2ee6571140767adfbb
Carlo Costantino <costantinoart@gmail.com>
2020-04-18 16:53:34 -1000

    Added transition to side menue

--- src/components/sideDrawer/SideDrawer.js
+++ src/components/sideDrawer/SideDrawer.js
@@ -1,20 +1,31 @@
 import React from "react";
+import StoreContext from "../../StoreContext";
 import { Link } from "react-router-dom";
 import "./sideDrawer.css";
 
-const SideDrawer = (props) => {
-  return (
-    <nav className="side-drawer">
-      <ul>
-        <li>
-          <Link to="/">About</Link>
-        </li>
-        <li>
-          <Link to="/Breathe">Begin</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+class SideDrawer extends React.Component {
+  static contextType = StoreContext;
+
+  render() {
+    let drawerClasses = "side-drawer";
+
+    if (this.context.sideDrawerOpen) {
+      drawerClasses = "side-drawer open";
+    }
+
+    return (
+      <nav className={drawerClasses}>
+        <ul>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+          <li>
+            <Link to="/Breathe">Begin</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 export default SideDrawer;
