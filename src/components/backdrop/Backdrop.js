497799cc7ecfd6068149a468fbe6bf5af1038a50
Carlo Costantino <costantinoart@gmail.com>
2020-04-18 16:54:19 -1000

    Added toggle functionality to backdrop

--- src/components/backdrop/Backdrop.js
+++ src/components/backdrop/Backdrop.js
@@ -1,8 +1,17 @@
 import React from "react";
+import StoreContext from "../../StoreContext";
 import "./backdrop.css";
 
-const Backdrop = (props) => {
-  return <div className="backdrop" />;
-};
+class Backdrop extends React.Component {
+  static contextType = StoreContext;
+  render() {
+    return (
+      <div
+        className="backdrop"
+        onClick={() => this.context.handleToggleSideDrawer()}
+      />
+    );
+  }
+}
 
 export default Backdrop;
