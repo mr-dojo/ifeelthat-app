a266679a2995c4d4ff203656468e418e4115fd19
Carlo Costantino <costantinoart@gmail.com>
2020-04-19 18:26:54 -1000

    Added styling and mediaqueries

--- src/components/toolBar/ToolBar.js
+++ src/components/toolBar/ToolBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DrawerToggleButton from "./drawerToggleButton/DrawerToggleButton";
+import { Link } from "react-router-dom";
 import "./toolBar.css";
 
 class ToolBar extends React.Component {
@@ -7,12 +8,22 @@ class ToolBar extends React.Component {
     return (
       <div className="tool-bar" role="navigation">
         <DrawerToggleButton />
-        <div className="spacer" />
         {this.props.location.pathname !== "/" ? (
           <span className="tool-bar__app-name">Listen.Share.</span>
         ) : (
           ""
         )}
+        <div className="spacer" />
+        <nav className="tool-bar__nav">
+          <ul>
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/breathe">Begin</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
