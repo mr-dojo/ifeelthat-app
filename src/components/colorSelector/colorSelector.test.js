bb3eeec549566a0df8e60661c9a80379b09f702c
Carlo Costantino <costantinoart@gmail.com>
2020-04-27 13:30:54 -1000

    Added smoke tests for all components

--- src/components/colorSelector/colorSelector.test.js
+++ src/components/colorSelector/colorSelector.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ToolBar from "./ToolBar";
+import ColorSelector from "./ColorSelector";
 import { BrowserRouter } from "react-router-dom";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
     <BrowserRouter>
-      <ToolBar location={{ pathname: "/" }} />
+      <ColorSelector />
     </BrowserRouter>,
     div
   );
--- src/components/sideDrawer/sideDrawer.test.js
+++ src/components/sideDrawer/sideDrawer.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ToolBar from "./ToolBar";
+import SideDrawer from "./SideDrawer";
 import { BrowserRouter } from "react-router-dom";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
     <BrowserRouter>
-      <ToolBar location={{ pathname: "/" }} />
+      <SideDrawer />
     </BrowserRouter>,
     div
   );
