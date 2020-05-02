ce4f6fe173c5f17cf1327c798aed315e1db8e1df
Carlo Costantino <costantinoart@gmail.com>
2020-05-02 08:51:04 -1000

    Styled code input

--- src/pages/share/shareAudio/ShareAudio.js
+++ src/pages/share/shareAudio/ShareAudio.js
@@ -77,34 +77,33 @@ class ShareAudio extends React.Component {
       <section>
         <div className="div-container_eighty-vh section_margin">
           <h2>How</h2>
-          <ol>
-            <li>Record a 60 second clip using your favorite audio recorder</li>
-            <li>
+          <div className="audio-instructions">
+            <p className="xtra-small-text">
+              Record a 60 second clip using your favorite audio recorder
+              <br />
               Upload it to SoundCloud as a private file and title it "
-              {this.context.feeling.color}+{this.context.feeling.emotion}"
-            </li>
-            <li>Click "Go to your track" >>> "Share" >>> "Embed"</li>
-            <li>
+              {this.context.feeling.emotion}"<br />
+              Click "Go to your track" >>> "Share" >>> "Embed"
+              <br />
               Copy the "Code" link and paste it below
-              <form onSubmit={(e) => this.handleLinkSubmit(e)}>
-                <label
-                  htmlFor="embeded-audio-link"
-                  aria-label="Embeded Audio Link Input"
-                ></label>
-                <input
-                  type="text"
-                  name="embeded-audio-link"
-                  id="iframe"
-                  placeholder="Paste the code here"
-                ></input>
-                <Button buttonText="Share" buttonType="submit" />
-                <Button
-                  buttonText="Cancel"
-                  onClick={(e) => this.handleCancel()}
-                />
-              </form>
-            </li>
-          </ol>
+            </p>
+          </div>
+
+          <form onSubmit={(e) => this.handleLinkSubmit(e)}>
+            <label
+              htmlFor="embeded-audio-link"
+              aria-label="Embeded Audio Link Input"
+            ></label>
+            <input
+              className="iframe-input"
+              type="text"
+              name="embeded-audio-link"
+              id="iframe"
+              placeholder="Paste the code here"
+            ></input>
+            <Button buttonText="Share" buttonType="submit" />
+            <Button buttonText="Cancel" onClick={(e) => this.handleCancel()} />
+          </form>
         </div>
       </section>
     );
