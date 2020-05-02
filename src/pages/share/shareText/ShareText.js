4b4d6a5ae340215af8a2752295e7e4f322334dd1
Carlo Costantino <costantinoart@gmail.com>
2020-05-02 07:55:28 -1000

    Fixed paragraph wording

--- src/pages/share/shareText/ShareText.js
+++ src/pages/share/shareText/ShareText.js
@@ -112,7 +112,7 @@ export default class ShareText extends React.Component {
       <section>
         <div className="div-container_eighty-vh section_margin">
           <p className="medium-text">
-            How you feel <strong>now?</strong>
+            How do you feel <strong>now?</strong>
           </p>
           <p className="small-text">What, if anything, is different?</p>
           <p className="xtra-small-text">
